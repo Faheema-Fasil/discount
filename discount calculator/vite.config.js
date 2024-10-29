24763cb2ee79e07052d22af7e5feddc8f1969088
Faheema_Fasil <faheemafasil12@gmail.com>
2024-10-29 22:14:04 +0530

    updates

--- discount calculator/vite.config.js
+++ discount calculator/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/discount/', // update this to match your repo name
+  base: '/discount/', 
 });
 
